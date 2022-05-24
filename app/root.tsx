3b67b717b4fa727d7032a6edc7288d2866a0d0a5
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2022-05-24 10:15:39 -0500

    Basic CSS and small code improvements

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,21 +8,43 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import globalCss from "./styles/global.css";
+import lightThemeCss from "./styles/themes/light.css";
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: globalCss,
+  },
+];
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <Meta />
+
+        <title>Name of Store</title>
+
+        <link rel="stylesheet" href={lightThemeCss} />
+
         <Links />
       </head>
+
       <body>
-        <Outlet />
+        <header>Header</header>
+
+        <main>
+          <Outlet />
+        </main>
+
+        <footer>Footer</footer>
+
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
